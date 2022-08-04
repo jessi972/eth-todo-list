73478f72ec941c88ffb412587247793cc9d1a117
jessi972 <saphyr.9@outlook.fr>
2022-08-04 18:05:00 +0200

    Initial commit

---
+++ contracts/TodoList.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+contract TodoList {
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() {
+        createTask("Check out dappuniv");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+}
